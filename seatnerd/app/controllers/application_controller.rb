bd1eebf9acb42ee36d7ef4c40063228eaf3ac20c
jesusmontano <jesusmontano96@gmail.com>
2019-07-02 16:41:19 -0700

    Update application_controller

--- seatnerd/app/controllers/application_controller.rb
+++ seatnerd/app/controllers/application_controller.rb
@@ -1,27 +1,35 @@
 class ApplicationController < ActionController::Base
-    helper_method :current_user, :signed_in?
+  protect_from_forgery with: :exception
 
-    private
-    def current_user
-        @current_user ||= User.find_by_session_token(session[:session_token])
-    end
+  helper_method :current_user, :logged_in?
 
-    def signed_in?
-        !!current_user
-    end
+  private
 
-    def sign_in(user)
-        @current_user = user
-        session[:session_token] = user.reset_token!
-    end
+  def current_user
+    return nil unless session[:session_token]
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
 
-    def sign_out
-        current_user.try(:reset_token!)
-        session[:session_token] = nil
-    end
+  def logged_in?
+    !!current_user
+  end
 
-    def require_signed_in!
-        redirect_to new_session_url unless signed_in?
+  def login(user)
+    user.reset_session_token!
+    session[:session_token] = user.session_token
+    @current_user = user
+  end
+
+  def logout
+    current_user.reset_session_token!
+    session[:session_token] = nil
+    @current_user = nil
+  end
+
+  def require_logged_in
+    unless current_user
+      render json: { base: ['invalid credentials'] }, status: 401
     end
-    
+  end
+  
 end
