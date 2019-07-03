2ec253e3b4961cc1af46a8d7f4b746c0562c47ad
jesusmontano <jesusmontano96@gmail.com>
2019-07-03 15:05:15 -0700

    Add Store

--- seatnerd/frontend/seatnerd.jsx
+++ seatnerd/frontend/seatnerd.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import { login, signup, logout } from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
+    let store = configureStore();
+    // Testing start
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+    window.login = login;
+    window.signup = signup;
+    window.logout = logout;
+    // Testing end
     const root = document.getElementById('root');
     ReactDOM.render(<h1>Welcome to my SeatGeek clone.</h1>, root);
 });
