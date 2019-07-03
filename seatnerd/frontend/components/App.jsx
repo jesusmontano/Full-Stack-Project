ce0c0fc6792a4588f45bceca98ab854619f90a7c
jesusmontano <jesusmontano96@gmail.com>
2019-07-03 15:18:34 -0700

    Update Entry Point

--- seatnerd/frontend/components/App.jsx
+++ seatnerd/frontend/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const App = () => (
     <div>
-        <h1>SeatNerd</h1>
+        <h1>SeatNerd from App component.</h1>
     </div>
 );
 
--- seatnerd/frontend/seatnerd.jsx
+++ seatnerd/frontend/seatnerd.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-import { login, signup, logout } from './util/session_api_util';
+import Root from './components/root';
+// import { login, signup, logout } from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
-    let store = configureStore();
+    const store = configureStore();
     // Testing start
     window.getState = store.getState;
     window.dispatch = store.dispatch;
-    window.login = login;
-    window.signup = signup;
-    window.logout = logout;
+    // window.login = login;
+    // window.signup = signup;
+    // window.logout = logout;
     // Testing end
     const root = document.getElementById('root');
-    ReactDOM.render(<h1>Welcome to my SeatGeek clone.</h1>, root);
+    ReactDOM.render(<Root store={store}/>, root);
 });
