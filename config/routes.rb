ed2d76d5ce4bb605325c8b240afb112763daa3c5
jesusmontano <jesusmontano96@gmail.com>
2019-07-06 17:37:59 -0700

    Add model and controller for teams

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy, :show]
     resources :events, only: [:index, :show]
+    resources :teams, only: [:index, :show]
   end
   
   root "static_pages#root"
