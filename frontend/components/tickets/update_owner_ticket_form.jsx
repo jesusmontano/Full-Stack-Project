9f91f7f99328d12e603bf1aa6ad88ee670cb8094
jesusmontano <jesusmontano96@gmail.com>
2019-08-05 15:06:00 -0700

    Begin checkout form

---
+++ frontend/components/tickets/update_owner_ticket_form.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class UpdateOwnerTicketForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = this.props.ticket;
+    }
+}
