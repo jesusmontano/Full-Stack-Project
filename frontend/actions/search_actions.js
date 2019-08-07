b74dd5a0ce6078d24c99ec5ce6111df918c97511
jesusmontano <jesusmontano96@gmail.com>
2019-08-07 11:00:27 -0700

    Add search actions

---
+++ frontend/actions/search_actions.js
@@ -0,0 +1,17 @@
+import * as SearchApiUtil from '../util/search_api_util';
+
+export const RECEIVE_RESULT = "RECEIVE_RESULT";
+
+const receiveResult = (results) => {
+    return ({
+        type: RECEIVE_RESULT,
+        results
+    })
+};
+
+export const getSearchResults = str => dispatch => {
+
+    return (
+    SearchApiUtil.getSearchResults(str)
+    .then((results) => dispatch(receiveResult(results))))
+}
