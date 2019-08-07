d5427b2c348c65fe7a71d84e7d990076e7669942
jesusmontano <jesusmontano96@gmail.com>
2019-08-07 10:25:35 -0700

    Add search route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do
+    get '/search', to: 'search#search'
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy, :show]
     resources :events, only: [:index, :show]
