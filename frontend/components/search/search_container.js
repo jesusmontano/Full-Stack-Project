cb4a8d86c4374be44cb8507a822bd16e452d1c09
jesusmontano <jesusmontano96@gmail.com>
2019-08-07 11:21:46 -0700

    Add search container

---
+++ frontend/components/search/search_container.js
@@ -0,0 +1,17 @@
+import { connect } from 'react-redux';
+import { getSearchResults } from '../../actions/search_actions';
+import Search from './search';
+
+const mapStateToProps = (state) => {
+
+    return {
+        results: state.results,
+        loggedIn: Boolean(state.session.id)
+    }
+}
+
+const mapDispatchToProps = dispatch => ({
+    getSearchResults: (str) => dispatch(getSearchResults(str))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Search); 
